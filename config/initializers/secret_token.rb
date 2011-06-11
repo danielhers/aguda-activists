bf7472aad4c5942fefd55653f2e870c9f0b136dd
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2011-06-11 09:03:03 +0300

    Switched to Ruby on Rails instead of PHP
    Old contents moved to old/
    New contents not implemented yet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgudaActivists::Application.config.secret_token = '635d49b11bf8173a0756311b4a23d545b7b1e5304253b72302f2e7608b3d2e4312690f4ffa0e54e08d3c25feac44377944127eb1f321b28c9c224721a8543ddb'
