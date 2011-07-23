04c522947a9b52da7241b283ea9cb4d77bda5d54
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2011-07-23 23:56:57 +0300

    Can now add new interests when creating/editing activists

--- app/helpers/activists_helper.rb
+++ app/helpers/activists_helper.rb
@@ -6,4 +6,15 @@ module ActivistsHelper
       false
     end
   end
+
+  def add_interest_link(name, form)
+    link_to_function name do |page|
+      interest = render :partial => 'new_interest', :object => Interest.new,
+        :locals => { :pf => form }
+      page << %{
+        var new_id = new Date().getTime();
+        $('#interests').append("#{ escape_javascript interest }".replace(/new_interest/g, new_id))
+      }
+    end
+  end
 end
